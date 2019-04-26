40139940e82e4808a504eee834ecd3b67756435a
Adam Nogradi <nogradiadam@gmail.com>
2019-04-26 23:37:40 +0100

    Initial working app analysing Twitter sentiment

---
+++ README.md
@@ -0,0 +1,9 @@
+#  Twittermenti
+
+## Twitter sentiment analyser app
+
+The app uses a pre-trained model to analyse the first 100 tweets given a handle or hashtag and judge the sentiment about it.
+
+This project was created as part of my Udemy course [iOS 12 & Swift - The Complete iOS App Development Bootcamp](https://www.udemy.com/ios-12-app-development-bootcamp/)
+
+It wasn't created for commercial purposes so please don't use it for such goals either.
---
+++ Twittermenti/ViewController.swift
@@ -0,0 +1,91 @@
+//
+//  ViewController.swift
+//  Twittermenti
+//
+//  Created by Angela Yu on 17/07/2018.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import SwifteriOS
+import CoreML
+import SwiftyJSON
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var sentimentLabel: UILabel!
+    
+    let sentimentClasssifier = TweetSentimentClassifier()
+    
+    var swifter : Swifter?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        readPropertyList()
+    }
+    
+    func readPropertyList() {
+        var format = PropertyListSerialization.PropertyListFormat.xml
+        var plistData:[String:AnyObject] = [:]
+        let plistPath:String? = Bundle.main.path(forResource: "TwitterCredentials", ofType: "plist")!
+        let plistXML = FileManager.default.contents(atPath: plistPath!)!
+        
+        do{
+            plistData = try PropertyListSerialization.propertyList(from: plistXML,options: .mutableContainersAndLeaves,format: &format)as! [String:AnyObject]
+            
+            swifter = Swifter(consumerKey: plistData["CONSUMER KEY"] as! String, consumerSecret: plistData["CONSUMER SECRET"] as! String)
+        }
+        catch{
+            print("Error reading plist: \(error), format: \(format)")
+        }
+    }
+
+    @IBAction func predictPressed(_ sender: Any) {
+        if let searchText = textField.text {
+            swifter?.searchTweet(using: searchText, lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
+                var tweets = [TweetSentimentClassifierInput]()
+                for i in 0..<100 {
+                    if let tweet = results[i]["full_text"].string {
+                        let tweetForClassification = TweetSentimentClassifierInput(text: tweet)
+                        tweets.append(tweetForClassification)
+                    }
+                }
+                do {
+                    var sentimentScore = 0
+                    let predictions = try self.sentimentClasssifier.predictions(inputs: tweets)
+                    for prediction in predictions {
+                        if prediction.label == "Pos" {
+                            sentimentScore += 1
+                        } else if prediction.label == "Neg" {
+                            sentimentScore -= 1
+                        }
+                    }
+                    if sentimentScore > 20 {
+                        self.sentimentLabel.text = "😍"
+                    } else if sentimentScore > 10 {
+                        self.sentimentLabel.text = "😀"
+                    } else if sentimentScore > 0 {
+                        self.sentimentLabel.text = "🙂"
+                    } else if sentimentScore == 0 {
+                        self.sentimentLabel.text = "😐"
+                    } else if sentimentScore > -10 {
+                        self.sentimentLabel.text = "🙁"
+                    } else if sentimentScore > -20 {
+                        self.sentimentLabel.text = "😡"
+                    } else {
+                        self.sentimentLabel.text = "🤮"
+                    }
+                    
+                } catch {
+                    print("There was an error making a prediction: \(error)")
+                }
+            }) { (error) in
+                print("Error with the Twitter API request: \(error)")
+            }
+        }
+    }
+    
+}
+
